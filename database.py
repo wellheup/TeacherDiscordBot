90424dd1492e3e197cad0a087b28c5ffc6b1fe58
Phillip Wellheuser <phillip.wellheuser@gmail.com>
2024-06-11 05:24:19 +0000

    another attempt to reconcile redeploy issues

--- database.py
+++ database.py
@@ -2,6 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import psycopg2
+try:
+	connection = psycopg2.connect(os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname"))
+	connection.close()
+	print("psycopg2 driver detected and working")
+except Exception as e:
+	raise ImportError("Error: psycopg2 not installed or database URL incorrect") from e
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname")
 
